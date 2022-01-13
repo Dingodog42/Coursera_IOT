8a4db7fe038555ddec75abb74fad48110a1365f1
apiccirillo80 <86972207+Dingodog42@users.noreply.github.com>
2022-01-13 15:47:21 -0500

    Coursera IOT Pi-Car Lab

---
+++ obstacle_avoidance_Lab.py
@@ -0,0 +1,26 @@
+import picar_4wd as fc
+import time
+
+speed = 30
+
+def main():
+    while True:
+        scan_list = fc.scan_step(35)
+        if not scan_list:
+            continue
+
+        tmp = scan_list[3:7]
+        print(tmp)
+        if tmp != [2,2,2,2]:
+            fc.stop()
+            fc.backward(speed)
+            time.sleep(1)
+            fc.turn_right(speed)
+        else:
+            fc.forward(speed)
+
+if __name__ == "__main__":
+    try: 
+        main()
+    finally: 
+        fc.stop()
